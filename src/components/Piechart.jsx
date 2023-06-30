72d1e1cbbc89964cbbd4e7c3923d7ca7b4c628c2
harshgupta20 <hgreatgupta2001@gmail.com>
2023-07-01 00:34:14 +0530

    Final enhancement in UI

--- src/components/Piechart.jsx
+++ src/components/Piechart.jsx
@@ -3,11 +3,10 @@ import { PieChart, Pie, Cell } from "recharts";
 const data = [
     { name: "Group A", value: 400 },
     { name: "Group B", value: 300 },
-    { name: "Group C", value: 300 },
-    { name: "Group D", value: 200 }
+    { name: "Group C", value: 300 }
 ];
 
-const COLORS = ["#0088FE", "#00C49F", "#FFBB28", "#FF8042"];
+const COLORS = ["#98D89E", "#F6DC7D", "#EE8484"];
 
 const RADIAN = Math.PI / 180;
 
